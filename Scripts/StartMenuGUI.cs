479ee3d93b9e3cc48ed21bac8bd6fc99d90c26e6
Kai Zhang <kylerzhang11@gmail.com>
2013-05-25 14:42:27 +0800

    Add a exit button in start menu. resolves #1

--- Scripts/StartMenuGUI.cs
+++ Scripts/StartMenuGUI.cs
@@ -7,5 +7,8 @@ public class StartMenuGUI : MonoBehaviour {
         if (GUI.Button (new Rect (10,10,100,20), "Load game")) {
             Application.LoadLevel(1);
         }
+        if (GUI.Button(new Rect(10, 35, 100, 20), "Exit")) {
+            Application.Quit();
+        }
     }
 }
